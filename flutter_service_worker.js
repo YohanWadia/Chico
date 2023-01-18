7f716d6c7261dd416bd32676a55ab581a3cd9223
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2023-01-18 23:05:17 +0200

    err2 upgrd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,9 +46,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f6ef73c9791771124ab7ec49fe909545",
-"/": "f6ef73c9791771124ab7ec49fe909545",
-"main.dart.js": "c99586cdc3da6d703fbdc3844b92aaaf",
+"index.html": "0a0302a0e02448c79bf2fe926eccfcc1",
+"/": "0a0302a0e02448c79bf2fe926eccfcc1",
+"main.dart.js": "2f6fa901ebbd0214be215ec71aefbb08",
 "manifest.json": "8c246b7f422fbed020a91009f9c14439",
 "version.json": "e00ccfbc9a495800f60930781214646a"
 };
